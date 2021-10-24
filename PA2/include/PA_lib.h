5f59ba1edba638a61d2dc6b872bb8371c4879d3c
AerGlass <86316669+AerGlass@users.noreply.github.com>
2021-10-24 10:50:43 -0500

    bloating spa even more

--- PA2/include/PA_lib.h
+++ PA2/include/PA_lib.h
@@ -104,8 +104,11 @@ b = (3 << 11);
 #include "PA_sprite3d.h"
 
 //do simplepalib things
+void SPA_SimpleText(cont char *print){
+iprintf(print);
+}
 void SPA_LoadSprite(int screen, int ramslot, int width, int height, const char *dir, const char *dir2, bool transflag) {
-    NF_LoadSpriteGfx(dir, ramslot, width, height);
+    PA_LoadSpriteGfx(dir, ramslot, width, height);
     NF_LoadSpritePal(dir2, ramslot);
     NF_VramSpriteGfx(screen, ramslot, ramslot, transflag);
     NF_VramSpritePal(screen, ramslot, ramslot);
