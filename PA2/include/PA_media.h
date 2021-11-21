d044a0fa8f1d5352affd189aebc3a5b331ed1808
AerGlass <86316669+AerGlass@users.noreply.github.com>
2021-11-20 19:23:40 -0500

    remove annoying lines

--- PA2/include/PA_media.h
+++ PA2/include/PA_media.h
@@ -5,23 +5,8 @@ extern "C" {
 #ifndef __PA_MEDIA_H__
 #define __PA_MEDIA_H__
 
-
-
-
-
-// NightFox LIB - Include de funciones de carga de archivos multimedia
-// Requiere DevkitARM
-// Codigo por Cesar Rincon "NightFox"
-// http://www.nightfoxandco.com/
-// Version 20140413
-
-
-
 // Includes devKitPro
 #include <nds.h>
-
-
-
 
 
 // Funcion PA_LoadBMP();
@@ -29,10 +14,6 @@ void PA_LoadBMP(const char* file, u8 slot);
 // Carga un archivo BMP de 8, 16 o 24 bits en un slot de imagenes de 16 bits.
 // Debes inicializar el modo 16 bits, el backbuffer y usar la funcion PA_Draw16bitsImage();
 // para mostrarlo.
-
-
-
-
 
 #endif
 
