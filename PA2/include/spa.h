9bc5e480b2d0c97bac8504351ea008222fc9c518
AerGlass <86316669+AerGlass@users.noreply.github.com>
2021-12-16 18:18:46 -0500

    simplify function more

--- PA2/include/spa.h
+++ PA2/include/spa.h
@@ -12,7 +12,7 @@ extern "C" {
 void SPA_Init();
 void SPA_WaitFor(int sec);
 void SPA_LoadSprite(int screen, int id, int width, int height, const char *dir, bool transflag);
-void SPA_LoadBackground(const char *dir, const char *name, int width, int height, int screen, int layer);
+void SPA_LoadBackground(const char *dir, int width, int height, int screen, int layer);
 void SPA_unloadbg(int screen, int layer, const char *name);
 void SPA_unloadsprite(int screen, int id);
 void SPA_Flush();
