50cbb96d1628be0a38ecc61e9018fae05b7517ab
AerGlass <86316669+AerGlass@users.noreply.github.com>
2022-01-20 15:56:52 -0500

    big change

--- examples/bg/source/main.c
+++ examples/bg/source/main.c
@@ -1,22 +1,22 @@
 /*
-PA2lib Template.
-This is ready to use, no need for installation or anything else.
-Enjoy making games
+PA2lib BG Example.
 
-You need to convert textures via GRIT, next you put them into the nitrofs folder
+You need to convert you textures using GRIT in oder to use it on this library
 
 By: Aerglass
-Date: 17/01/22
+Date: 20/01/22
 */
+
 #include <stdio.h>				// Include C
 #include <nds.h>				//include Libnds
 #include <PA_lib.h>			 	//include PA2lib
 
 //main block of the program.
+
 int main(int argc, char **argv) {
-	
+
 	SPA_Init();				//inits everything
-	
+	SPA_LoadBackground("PA2lib", "bg", 256, 256, 0, 3); //load and display background
 	//loop for ever.
 	while(1) {
 		scanKeys();			//scan keys
