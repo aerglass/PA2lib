8bee1d45b269b1a57e0ca9ab39e60c643d3875db
AerGlass <86316669+AerGlass@users.noreply.github.com>
2022-03-05 20:07:38 -0500

    restore that

--- PA2/source/spa.c
+++ PA2/source/spa.c
@@ -41,7 +41,7 @@ void SPA_WaitFor(int sec){
 }
 
 //SPA_LoadBg
-void SPA_LoadBg(const char *dir, int width, int height, int screen, int layer) {
+void SPA_LoadBg(const char *dir, const char *name, int width, int height, int screen, int layer) {
     PA_LoadBg(dir, dir, width, height); //loads background
     PA_CreateBg(screen, layer, dir); //creates background
 }
